2bf76f28e01f9bae159063ab75aa3e4344d86fff
Sifan Jiang <jasper.sifan@gmail.com>
2019-12-02 01:40:50 +0100

    Working on plotting functions.

--- assignment-1.py
+++ assignment-1.py
@@ -8,12 +8,10 @@ from scipy.spatial.distance import cdist
 from scipy.stats import multivariate_normal as mvn
 
 W = np.array([[0.5], [-1.5]])
-X = np.linspace(-1.0, 1.0, num=201)
-
+X_Set = np.linspace(-1.0, 1.0, num=201)
 epsilon_sigma = 0.2
 
-
-def plot_2d_gaussian(mu, Sigma, size=3.0, interval=0.01, filename="2d_guassian"):
+def plot_2d_gaussian(mu, Sigma, size=3.0, interval=0.01, filename="2d_guassian", save=True):
     rv = mvn(mu, Sigma)
     x, y = np.mgrid[0-size : 0+size : interval, 0-size : 0+size : interval]
     xy = np.dstack([x, y])
@@ -23,42 +21,54 @@ def plot_2d_gaussian(mu, Sigma, size=3.0, interval=0.01, filename="2d_guassian")
     # CS = ax.contourf(x, y, rv.pdf(X))
     ax.clabel(CS, inline=0.5, fontsize=8)
     ax.set_aspect('equal', 'box')
-    tikz.save(filename+".tex")
+    if save:
+        tikz.save(filename+".tex")
+    else:
+        plt.show()
 
-def linear_function(W, x, epsilon):
-    return np.dot(x, W) + epsilon
+def plot_function(mu, Sigma):
+    W = np.random.multivariate_normal(mu, Sigma)
+    u"""
+    Working here.
+    """
+    _, ax = plt.subplots()
+    ax.axis('equal')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+
+def linear_function(W_Model, x, epsilon):
+    return np.dot(x, W_Model) + epsilon
 
 def generate_data_point():
     epsilon = np.array([[np.random.normal(0.0, epsilon_sigma)]])
-    x = np.array([[random.choice(X), 1.0]])
+    x = np.array([[random.choice(X_Set), 1.0]])
     t = linear_function(W, x, epsilon)
     return x, t
 
-def update_posterior(mu_1, Sigma_1, mu_2, Sigma_2):
-    Sigma_1_2_inverse = np.linalg.inv(Sigma_1 + Sigma_2)
-    Sigma_3 = np.dot(np.dot(Sigma_1, Sigma_1_2_inverse), Sigma_2)
-    mu_3 = np.dot(np.dot(Sigma_2, Sigma_1_2_inverse), mu_1) \
-        + np.dot(np.dot(Sigma_1, Sigma_1_2_inverse), mu_2)
-    return mu_3, Sigma_3
+def get_posterior(X, T, Sigma_prior_inverse):
+    Sigma_posterior_inverse = np.dot(np.transpose(X), X) / (epsilon_sigma**2) + Sigma_prior_inverse
+    Sigma_posterior = np.linalg.inv(Sigma_posterior_inverse)
+    mu_posterior = np.dot(np.dot(Sigma_posterior, np.transpose(X)), T) / (epsilon_sigma**2)
+    mu_posterior = np.transpose(mu_posterior)[0,:]
+    return mu_posterior, Sigma_posterior
 
 def question_9(i):
     mu_prior = np.zeros(2)
     Sigma_prior = np.eye(2)
     Sigma_prior_inverse = np.linalg.inv(Sigma_prior)
     if i is 1:
-        plot_2d_gaussian(mu_prior, Sigma_prior, filename="Q9-1")
-    elif i is 2 or 3:
-        x, t = generate_data_point()
-        Sigma_posterior_inverse = np.dot(np.transpose(x), x) / (epsilon_sigma**2) + Sigma_prior_inverse
-        Sigma_posterior = np.linalg.inv(Sigma_posterior_inverse)
-        mu_posterior = np.dot(Sigma_posterior, np.transpose(x)) * t / (epsilon_sigma**2)
-        mu_posterior = np.transpose(mu_posterior)[0,:]
-        if i is 2:
-            plot_2d_gaussian(mu_posterior, Sigma_posterior, filename="Q9-2")
-
+        plot_2d_gaussian(mu_prior, Sigma_prior, filename="Q9-1", save=False)
+    elif i is 2 or 3 or 4:
+        X, T = generate_data_point()
+        mu_posterior, Sigma_posterior = get_posterior(X, T, Sigma_prior_inverse)
+        plot_2d_gaussian(mu_posterior, Sigma_posterior, filename="Q9-2", save=False)
         for n in range(6):
-            
-            pass
+            x, t = generate_data_point()
+            X = np.vstack([X, x])
+            T = np.vstack([T, t])
+            mu_posterior, Sigma_posterior = get_posterior(X, T, Sigma_prior_inverse)
+            plot_2d_gaussian(mu_posterior, Sigma_posterior, filename="Q9-3-"+str(n+2), save=False)
+
 
 if __name__ == "__main__":
-    question_9(2)
+    question_9(3)
